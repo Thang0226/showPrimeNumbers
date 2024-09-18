65cff1f9dd0c3177b048b2d38f66e91ad6de39fb
Thang0226 <thang.nd0226@gmail.com>
2024-09-18 09:23:30 +0700

    add explanation

--- prime_number.js
+++ prime_number.js
@@ -1,9 +1,11 @@
+// Take input from user
 let n;
 do {
   n = prompt("Nhập vào số số nguyên tố muốn in ra:");
 } while (n <= 0);
 
-let N = 2;
+// Main function to print n prime numbers
+let N = 2; // Variable to find prime number, starting from 2
 for (let i = 0; i < n; i++) {
   while (!isPrime(N)) {
     N++;
@@ -12,12 +14,13 @@ for (let i = 0; i < n; i++) {
   N++;
 }
 
+// Function that detects a number is prime number or not, starting from 2
 function isPrime(num) {
-  if (num == 2 || num == 3 || num == 5 || num == 7 || num == 11) return true;
+  if (num == 2 || num == 3 || num == 5 || num == 7) return true;
   if (num == 4 || num == 6 || num == 8 || num == 9) return false;
   if (num % 2 == 0) return false;
 
-  // With odd number greater than 9:
+  // With odd numbers greater than 9:
   max_divisor = Math.floor(Math.sqrt(num)) + 1;
   for (let j = 3; j < max_divisor; j += 2) {
     if (num % j == 0) return false;
